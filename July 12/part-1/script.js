700c6197a594c17c722d2567486bc1fd3e3aaeb0
Mohd Saif <mohdsaifsiddique.feaindia@gmail.com>
2024-07-12 13:17:29 +0530

    July 12

---
+++ July 12/part-1/script.js
@@ -0,0 +1,26 @@
+/* 
+Class is an object template that defines the properties and methods that an object will have
+*/
+
+/* class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+}
+
+let student1 = new Student("Saket", 21, 10);
+let student2 = new Student("Vishal", 20, 9); */
+
+let student1 = {
+  name: "Saket",
+  age: 21,
+  grade: 10
+}
+
+let student2 = {
+  name: "Vishal",
+  age: 20,
+  grade: 9
+}
